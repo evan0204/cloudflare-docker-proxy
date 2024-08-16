2b1c6b8f454af3ccad2b7eb93bebf02ddd2fb5a0
Evan <huangdou0204@163.com>
2024-08-17 00:43:51 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ndsn.cn": dockerHub,
+  "quay.ndsn.cn": "https://quay.io",
+  "gcr.ndsn.cn": "https://gcr.io",
+  "k8s-gcr.ndsn.cn": "https://k8s.gcr.io",
+  "k8s.ndsn.cn": "https://registry.k8s.io",
+  "ghcr.ndsn.cn": "https://ghcr.io",
+  "cloudsmith.ndsn.cn": "https://docker.cloudsmith.io",
+  "ecr.ndsn.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ndsn.cn": dockerHub,
 };
 
 function routeByHosts(host) {
